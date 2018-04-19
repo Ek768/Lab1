10275bd859896171154c70c03a8fec4adeb61145
Ekaterina Bazhina <bazhina.ekaterina18@yandex.ru>
2018-04-19 13:22:55 +0300

    Начальные исходники (первый коммит)

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pctDraw = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.cmbM = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctDraw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(427, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "    Построить график : \r\nx = a*sin(3*t), y = b*cos(m*t)\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(437, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Выберете значение m:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(419, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Изменить цвет фона";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(419, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Изменить цвет графика";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(419, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Построить график";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(430, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Введите толщину линий:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(446, 218);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(419, 305);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Выход";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pctDraw
+            // 
+            this.pctDraw.BackColor = System.Drawing.SystemColors.Window;
+            this.pctDraw.Location = new System.Drawing.Point(11, 12);
+            this.pctDraw.Name = "pctDraw";
+            this.pctDraw.Size = new System.Drawing.Size(402, 316);
+            this.pctDraw.TabIndex = 10;
+            this.pctDraw.TabStop = false;
+            // 
+            // cmbM
+            // 
+            this.cmbM.FormattingEnabled = true;
+            this.cmbM.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cmbM.Location = new System.Drawing.Point(446, 77);
+            this.cmbM.Name = "cmbM";
+            this.cmbM.Size = new System.Drawing.Size(100, 21);
+            this.cmbM.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 351);
+            this.Controls.Add(this.cmbM);
+            this.Controls.Add(this.pctDraw);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Построение графика функции";
+            ((System.ComponentModel.ISupportInitialize)(this.pctDraw)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pctDraw;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox cmbM;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.cmbM.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+        private void Calculation(double t, out double x, out double y)
+        {
+            string m1 = cmbM.SelectedItem.ToString();
+            int m = Convert.ToInt32(m1);
+            x = 160 * (Math.Sin(3*t));
+            y = 110 * (Math.Cos(m*t));
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double h = 0.02f, k_max = 0, tt, x1, y1, x2, y2;
+            for (double t = 0; t <= 2 * Math.PI; t = t + h)
+            {
+                Calculation(t, out x2, out y2);
+                if (t == 0)
+                    k_max = (x2 > y2) ? x2 : y2;
+                else
+                {
+                    tt = (x2 > y2) ? x2 : y2;
+                    k_max = (tt > k_max) ? tt : k_max;
+                }
+            }
+            int pict_size = (pctDraw.Width < pctDraw.Height) ? pctDraw.Width : pctDraw.Height;
+            pict_size /= 2;
+            //Подготовка к рисованию            
+            Graphics g = pctDraw.CreateGraphics();
+            g.Clear(button1.BackColor);
+            g.ScaleTransform(pict_size / (float)k_max, pict_size / (float)k_max);
+            g.TranslateTransform(pctDraw.Width / 2, pctDraw.Height / 2, System.Drawing.Drawing2D.MatrixOrder.Append);
+            Pen p1 = new Pen(Color.Black);
+            // Оси координат
+            g.DrawLine(p1, -pctDraw.Width / 2, 0, pctDraw.Width / 2, 0);
+            g.DrawLine(p1, 0, -pctDraw.Height / 2, 0, pctDraw.Height / 2);
+            // Рисование графика функции
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show(this, "Введите толщину линии", "Сообщение об ошибке", MessageBoxButtons.OK, MessageBoxIcon.Stop); return;
+            }
+            double Size = Convert.ToDouble(textBox2.Text);
+            Pen p = new Pen(button2.BackColor, (float)Size);
+            Calculation(0, out x1, out y1);
+            double xmax = 0, ymax = 0;
+            for (double t = h; t <= 2 * Math.PI + h; t = t + h)
+            {
+                Calculation(t, out x2, out y2);
+                g.DrawLine(p, (float)x1, (float)y1, (float)x2, (float)y2);
+                x1 = x2;
+                y1 = y2;
+                xmax = (x1 > xmax) ? x1 : xmax;
+                ymax = (y1 > ymax) ? y1 : ymax;
+            }
+            // Подпись осей координат
+            g.DrawString((xmax).ToString(), new System.Drawing.Font("Arial", 8), new SolidBrush(Color.Black), (float)xmax, 0);
+            g.DrawString((ymax).ToString(), new System.Drawing.Font("Arial", 8), new SolidBrush(Color.Black), 0, (float)-ymax);
+        }
+        //Изменение цвета фона
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            dlg.Color = button1.BackColor;
+            if (dlg.ShowDialog() == DialogResult.OK)
+                button1.BackColor = dlg.Color;
+        }
+        //Изменение цвета графика
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlgGraph = new ColorDialog();
+            dlgGraph.Color = button2.BackColor;
+            if (dlgGraph.ShowDialog() == DialogResult.OK)
+                button2.BackColor = dlgGraph.Color;
+        }     
+        //Выход
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
